029f7ef04a8254180023bd01153068dd4d06f12b
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-06 19:10:52 +0500

    Write test for calculate.js

---
+++ src/__Tests__/calculate.test.js
@@ -0,0 +1,23 @@
+import calculate from '../logic/calculate';
+import operate from '../logic/operate';
+
+describe('Tests for Logic files ', () => {
+  test('tests calculate.js', () => {
+    expect(
+      calculate(
+        {
+          total: 52,
+          next: null,
+          operation: null,
+        },
+        'AC'
+      )
+    ).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
+
+  
+});
