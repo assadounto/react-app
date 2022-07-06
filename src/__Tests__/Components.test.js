010e5c6f7fad75748b82c46e3f450ffd7fa485a6
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-06 18:57:20 +0500

    Refactor tests for componenets

---
+++ src/__Tests__/Components.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Home from '../components/Home';
+import Calculator from '../components/calculator';
+import Quotes from '../components/Quotes';
+
+it('Home renders correctly', () => {
+  const tree = renderer
+    .create(<Home />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Calculator renders correctly', () => {
+  const tree1 = renderer
+    .create(<Calculator />)
+    .toJSON();
+  expect(tree1).toMatchSnapshot();
+});
+
+it('Quotes renders correctly', () => {
+  const tree2 = renderer
+    .create(<Quotes />)
+    .toJSON();
+  expect(tree2).toMatchSnapshot();
+});
